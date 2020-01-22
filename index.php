50841b40ee4a6de5a2e2a6b1bf6ed4110e404ae1
f-koki <fukushimakoki0108@gmail.com>
2020-01-22 22:59:55 +0900

    feat: try line break and display date time

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
-  echo 'hello world';
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  $a = nl2br('hello world' . "\n");
+  echo $a;
+  echo date('Y-m-d H:i:s') . '現在';
+  ?>
+</body>
+
+</html>
